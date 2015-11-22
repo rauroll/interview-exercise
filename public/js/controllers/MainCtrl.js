eca35af89e87c24135700008b81871769553b598
ollir <olli.rauramo@aalto.fi>
2015-11-23 00:28:35 +0200

    ui refinements, frontend improvements

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,23 +1,35 @@
 angular.module('myApp').controller('MainController', ['MessageService', '$scope', '$rootScope', function(MessageService, $scope, $rootScope) {
 
-	$scope.variable = "Hello world!";
-	$scope.updating;
-
 	$scope.messages = ["Fetching..."];
 
-	MessageService.getMessages().then(function(messages) {
-		if (messages.length == 0) {
-			$scope.messages = ["No world saving strings found"];
-		} else {
-			$scope.messages = messages;
-		}
-	});
 
+	
 	$scope.send = function(message) {
 		MessageService.sendMessage($scope.myForm.message).then(function(data) {
+			$scope.myForm = {};
 			MessageService.getMessages().then(function(data) {
 				$scope.messages = data;
 			});
+		})
+		.catch(function() {
+			$scope.error = true;
+			$scope.errorMessage = "Failed to send the message."
+			// todo, show errormessages
 		});
 	}
+
+	$scope.getMessages = function() {
+		$scope.loading = true;
+		MessageService.getMessages().then(function(messages) {
+			$scope.loading = false;
+			if (messages.length == 0) {
+				$scope.messages = ["No world saving strings found"];
+			} else {
+				$scope.messages = messages;
+			}
+		});
+	}
+
+	$scope.getMessages();
+
 }])
