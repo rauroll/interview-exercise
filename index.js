0154a3825bf772af315935d057e60500894e0c0a
ollir <olli.rauramo@aalto.fi>
2015-11-22 14:20:51 +0200

    backend boilerplate code, nodejs can now serve the index file

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var path = require('path');
+
+
+var port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/*', function(req, res) {
+	res.send('index');
+});
+
+app.listen(port);
+console.log("Listening to port 3000.");
+
+exports = module.exports = app;
